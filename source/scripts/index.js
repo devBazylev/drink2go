171aa9bbca2dcac84628717dc76f3db29feed659
jar <dev.bazylev@gmail.com>
2024-02-24 22:33:38 +0300

    add bad working burger menu

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,10 @@
-/* в этот файл добавляет скрипты*/
+const nav = document.querySelector('.header__navigation');
+const burger = document.querySelector('.header__toggle');
+
+nav.classList.toggle('header__navigation--closed');
+nav.classList.toggle('header__navigation--opened');
+
+burger.onclick = function() {
+  nav.classList.toggle('header__navigation--closed');
+  nav.classList.toggle('header__navigation--opened');
+};
