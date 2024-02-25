4b9f5b9b79a0f89962a5911a3ad296f11564c558
jar <dev.bazylev@gmail.com>
2024-02-25 17:35:28 +0300

    add mobile menu + hero + features + part of catalog

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,9 +1,6 @@
 const nav = document.querySelector('.header__navigation');
 const burger = document.querySelector('.header__toggle');
 
-nav.classList.toggle('header__navigation--closed');
-nav.classList.toggle('header__navigation--opened');
-
 burger.onclick = function() {
   nav.classList.toggle('header__navigation--closed');
   nav.classList.toggle('header__navigation--opened');
