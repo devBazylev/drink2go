8ab1db66b0062c819ca444a5af057f1a3c3e4e5e
jar <dev.bazylev@gmail.com>
2024-03-04 14:48:22 +0300

    add function changeAllPaths

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,21 +1,12 @@
 import { burgerHandler } from './burger.js';
 import { changePrice } from './no-ui-slider.js';
+import { changeAllPaths } from './slider.js';
+
+const inputs = document.querySelectorAll('.slider-input');
 
 burgerHandler();
 changePrice();
 
-const image = document.querySelector('.slider-image');
-const inputs = document.querySelectorAll('.slider-input');
-
-let index = 0;
-let path = image.src;
-
-const changeId = function () {
-  index = this.id;
-  path = path.replace(/slide-[0-2]/, `slide-${index}`);
-  // console.log(path);
-};
-
 inputs.forEach((element) => {
-  element.addEventListener('change', changeId);
+  element.addEventListener('change', changeAllPaths);
 });
--- source/scripts/slider.js
+++ source/scripts/slider.js
@@ -1,14 +1,20 @@
-let a = 0;
+const image = document.querySelector('.slider__image');
+const sources = Array.from(document.querySelectorAll('.slider__picture'));
 
-const images = document.querySelectorAll('.slider-image');
+let index = 0;
+let pathImg = image.src;
+let pathImgMinor = image.srcset;
 
-const toggle = () => {
-  images[a].classList.toggle('slider-image--js');
-  a = a + 1;
-  images[a].classList.toggle('slider-image--js');
+const changeAllPaths = function () {
+  index = this.id;
+  pathImg = pathImg.replace(/slide-[0-2]/, `slide-${index}`);
+  pathImgMinor = pathImgMinor.replace(/slide-[0-2]/, `slide-${index}`);
+  image.setAttribute('src', pathImg);
+  image.setAttribute('srcset', pathImgMinor);
+  sources.forEach((source) => {
+    const sourcePath = source.srcset.replace(/slide-[0-2]/g, `slide-${index}`);
+    source.setAttribute('srcset', sourcePath);
+  });
 };
 
-export { toggle };
-
-// toggle();
-// import { toggle } from './slider.js';
+export { changeAllPaths };
